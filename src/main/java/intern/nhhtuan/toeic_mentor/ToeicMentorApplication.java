25d0c7f06263e33b9cad1e620424bcab8679cee7
intern.nhhtuan <nhhtuan-batch48@sdc.edu.vn>
2025-06-04 09:05:19 +0700

    first commmit

---
+++ src/main/java/intern/nhhtuan/toeic_mentor/ToeicMentorApplication.java
@@ -0,0 +1,13 @@
+package intern.nhhtuan.toeic_mentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicMentorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToeicMentorApplication.class, args);
+    }
+
+}
