3a7e1763a907d26f5d512be51d37e309abbf279a
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-06-05 09:02:51 +0700

    feat: create role admin and user when running

--- src/main/java/intern/nhhtuan/toeic_mentor/ToeicMentorApplication.java
+++ src/main/java/intern/nhhtuan/toeic_mentor/ToeicMentorApplication.java
@@ -1,7 +1,12 @@
 package intern.nhhtuan.toeic_mentor;
 
+import intern.nhhtuan.toeic_mentor.entity.ERole;
+import intern.nhhtuan.toeic_mentor.entity.Role;
+import intern.nhhtuan.toeic_mentor.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ToeicMentorApplication {
@@ -10,4 +15,16 @@ public class ToeicMentorApplication {
         SpringApplication.run(ToeicMentorApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner initRoles(RoleRepository roleRepository) {
+        return args -> {
+            for (ERole roleName : ERole.values()) {
+                roleRepository.findByName(roleName).orElseGet(() -> {
+                    Role role = new Role();
+                    role.setName(roleName);
+                    return roleRepository.save(role);
+                });
+            }
+        };
+    }
 }
