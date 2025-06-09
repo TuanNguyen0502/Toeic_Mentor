f09b9bf124cd11c2991c84152404e06568f90b1d
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-06-09 10:31:45 +0700

    feat: using System Message

--- src/main/java/intern/nhhtuan/toeic_mentor/controller/user/ChatController.java
+++ src/main/java/intern/nhhtuan/toeic_mentor/controller/user/ChatController.java
@@ -1,38 +1,21 @@
 package intern.nhhtuan.toeic_mentor.controller.user;
 
 import intern.nhhtuan.toeic_mentor.service.implement.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequiredArgsConstructor
 public class ChatController {
-    private final ChatClient chatClient;
     private final ChatService chatService;
-
-    public ChatController(ChatService chatService) {
-        this.chatService = chatService;
-        this.chatClient = chatService.getChatClient();
-    }
 
     @GetMapping("/stream")
     public Flux<String> chatWithStream(@RequestParam String message,
                                        @RequestParam String email) {
-        // Sử dụng Flux để trả về phản hồi theo luồng
-//        Flux<String> chatHistory = chatService.getChatHistory(email);
-//        Flux<String> response = chatClient.prompt()
-//                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, email))
-//                .user(message)
-//                .stream()
-//                .content();
-//        return Flux.concat(response, chatHistory);
-        return chatClient.prompt()
-                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, email))
-                .user(message)
-                .stream()
-                .content();
+        return chatService.getChatResponse(message, email);
     }
 }
--- src/main/java/intern/nhhtuan/toeic_mentor/service/implement/ChatService.java
+++ src/main/java/intern/nhhtuan/toeic_mentor/service/implement/ChatService.java
@@ -7,17 +7,24 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class ChatService {
     @Getter
     private final ChatClient chatClient;
     private final JdbcChatMemoryRepository jdbcChatMemoryRepository;
+
+    @Value("classpath:/prompts/system-message.st")
+    private Resource systemMessageResource;
 
     public ChatService(ChatClient.Builder builder, JdbcChatMemoryRepository jdbcChatMemoryRepository) {
         this.jdbcChatMemoryRepository = jdbcChatMemoryRepository;
@@ -27,6 +34,16 @@ public class ChatService {
         this.chatClient = builder
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
+    }
+
+    public Flux<String> getChatResponse(String message, String conversationId) {
+        var systemMessage = new SystemMessage(systemMessageResource);
+        var userMessage = new UserMessage(message);
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+        return chatClient.prompt(prompt)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .stream()
+                .content();
     }
 
     public Flux<String> getChatHistory(String conversationId) {
