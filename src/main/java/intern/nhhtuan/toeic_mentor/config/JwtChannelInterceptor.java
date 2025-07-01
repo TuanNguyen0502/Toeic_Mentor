6236887c9ee35db1cd6f3b8e0a480fb29da0867a
ldmvuong <minhvuongakira@gmail.com>
2025-07-01 08:55:06 +0700

    feat: config websocket

---
+++ src/main/java/intern/nhhtuan/toeic_mentor/config/JwtChannelInterceptor.java
@@ -0,0 +1,47 @@
+package intern.nhhtuan.toeic_mentor.config;
+
+import intern.nhhtuan.toeic_mentor.service.implement.JwtService;
+import intern.nhhtuan.toeic_mentor.service.implement.UserDetailServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtChannelInterceptor implements ChannelInterceptor {
+
+    private final JwtService jwtService;
+    private final UserDetailServiceImpl userDetailsService;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String token = accessor.getFirstNativeHeader("Authorization");
+            if (token != null && token.startsWith("Bearer ")) {
+                token = token.substring(7);
+                try {
+                    String email = jwtService.extractUsername(token);
+                    if (email != null) {
+                        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+                        if (jwtService.isTokenValid(token, userDetails)) {
+                            UsernamePasswordAuthenticationToken auth =
+                                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                            accessor.setUser(auth);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("WebSocket JWT invalid: " + e.getMessage());
+                }
+            }
+        }
+        return message;
+    }
+}
