7d63654ba56091ea84bd500ec0a9b23a2fcebba9
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-07-04 18:42:35 +0700

    feat: show admin setting page

---
+++ src/main/java/intern/nhhtuan/toeic_mentor/controller/admin/SettingController.java
@@ -0,0 +1,17 @@
+package intern.nhhtuan.toeic_mentor.controller.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/settings")
+@RequiredArgsConstructor
+public class SettingController {
+
+    @GetMapping("")
+    public String settings() {
+        return "admin/setting";
+    }
+}
