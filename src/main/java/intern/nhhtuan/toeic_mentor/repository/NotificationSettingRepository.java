65ac5f19bc311cc7576c3b4fe6e12d316bd293a0
ldmvuong <minhvuongakira@gmail.com>
2025-07-07 14:24:39 +0700

    feat: check user notification setting

--- src/main/java/intern/nhhtuan/toeic_mentor/repository/NotificationSettingRepository.java
+++ src/main/java/intern/nhhtuan/toeic_mentor/repository/NotificationSettingRepository.java
@@ -1,7 +1,20 @@
 package intern.nhhtuan.toeic_mentor.repository;
 
 import intern.nhhtuan.toeic_mentor.entity.NotificationSetting;
+import intern.nhhtuan.toeic_mentor.entity.enums.ENotificationTypeAction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface NotificationSettingRepository extends JpaRepository<NotificationSetting, Long> {
+    @Query("""
+    SELECT ns.enabled
+    FROM NotificationSetting ns
+    WHERE ns.user.id = :userId
+      AND ns.notificationType.action = :action
+    """)
+    Optional<Boolean> isEnabled(@Param("userId") Long userId, @Param("action") ENotificationTypeAction action);
+
 }
