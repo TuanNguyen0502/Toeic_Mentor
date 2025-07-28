67df2a9acdc9f5842ae74e31a51314c46b614614
Tuan <nguyenhahongtuan@gmail.com>
2025-07-28 10:46:31 +0700

    feat: create user_statistics table

---
+++ src/main/resources/db/migration/V7.1__create_user_statistics_table.sql
@@ -0,0 +1,12 @@
+USE
+`toeic-mentor`;
+
+CREATE TABLE user_statistics
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id         BIGINT NOT NULL,
+    estimated_score INT      DEFAULT 0,
+    score_interval  INT      DEFAULT 0,
+    created_at      DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
