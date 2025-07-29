1874631b94ffa907de62c226a42d3c6eecb10b06
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-07-29 14:58:13 +0700

    feat: enable asynchronous processing in the application

--- src/main/java/intern/nhhtuan/toeic_mentor/ToeicMentorApplication.java
+++ src/main/java/intern/nhhtuan/toeic_mentor/ToeicMentorApplication.java
@@ -2,10 +2,12 @@ package intern.nhhtuan.toeic_mentor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class ToeicMentorApplication {
 
     public static void main(String[] args) {
