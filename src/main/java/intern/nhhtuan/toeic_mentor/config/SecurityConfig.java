53a4fbc0b129dc14d704e967d1c778ad79667d3b
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-07-31 16:31:25 +0700

    feat: managing daily goals and integrate goal creation

--- src/main/java/intern/nhhtuan/toeic_mentor/config/SecurityConfig.java
+++ src/main/java/intern/nhhtuan/toeic_mentor/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                         .requestMatchers("/admin/asset/**").hasRole("ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN") // Allow access to admin routes for ADMIN role
                         .requestMatchers("/chat", "/chat/*", "/profile", "/change-password", "/home",
-                                "/statistics/**", "/study-streaks/**").hasAnyRole("USER", "ADMIN") // Allow access to root for USER and ADMIN roles
+                                "/statistics/**", "/study-streaks/**", "/goals/**")
+                        .hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated() // Require authentication for all other requests
                 )
                 .sessionManagement(session ->
---
+++ src/main/java/intern/nhhtuan/toeic_mentor/controller/user/GoalController.java
@@ -0,0 +1,46 @@
+package intern.nhhtuan.toeic_mentor.controller.user;
+
+import intern.nhhtuan.toeic_mentor.dto.request.GoalCreateRequest;
+import intern.nhhtuan.toeic_mentor.dto.response.GoalResponse;
+import intern.nhhtuan.toeic_mentor.service.interfaces.IGoalService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/goals")
+@RequiredArgsConstructor
+public class GoalController {
+    private final IGoalService goalService;
+
+    @GetMapping("/today")
+    public ResponseEntity<List<GoalResponse>> todayGoals() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).build(); // Unauthorized
+        }
+        String email = authentication.getName();
+        List<GoalResponse> goals = goalService.getTodayGoals(email);
+        return ResponseEntity.ok(goals);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<String> createGoal(@Valid @RequestBody GoalCreateRequest goalCreateRequest) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).build(); // Unauthorized
+        }
+        String email = authentication.getName();
+        boolean isCreated = goalService.createGoal(email, goalCreateRequest);
+        if (isCreated) {
+            return ResponseEntity.ok("Goal created successfully");
+        } else {
+            return ResponseEntity.status(400).body("Failed to create goal");
+        }
+    }
+}
