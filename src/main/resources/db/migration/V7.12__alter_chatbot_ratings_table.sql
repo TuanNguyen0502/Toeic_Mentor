957b6bc1b077282d4d0ace2c31613c90b2cbee72
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-08-08 10:10:41 +0700

    feat: update database

--- src/main/resources/db/migration/V7.12__alter_chatbot_ratings_table.sql
+++ src/main/resources/db/migration/V7.12__alter_chatbot_ratings_table.sql
@@ -12,5 +12,5 @@ CREATE TABLE chatbot_ratings
     rating     ENUM('LIKE', 'DISLIKE') NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (message_id) REFERENCES spring_ai_chat_memory_enhanced (id)
+    FOREIGN KEY (message_id) REFERENCES spring_ai_chat_memory_enhanced (id) ON DELETE SET NULL
 );
