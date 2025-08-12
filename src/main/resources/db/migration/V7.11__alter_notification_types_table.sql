2456df1c00dd15154f4902f14975da8d77a1460c
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-08-12 10:56:06 +0700

    feat: update notification types to include new streak achievement and revocation actions

--- src/main/resources/db/migration/V7.11__alter_notification_types_table.sql
+++ src/main/resources/db/migration/V7.11__alter_notification_types_table.sql
@@ -3,3 +3,6 @@ USE
 
 ALTER TABLE notification_types
     MODIFY COLUMN action ENUM('NEW_REPORT', 'COMPLETE_REPORT', 'NEW_STREAK_ACHIEVEMENT', 'STREAK_ACHIEVE_REVOKED');
+
+INSERT INTO notification_types (action, description)
+VALUES ('STREAK_ACHIEVE_REVOKED', 'User had a milestone revoked');
--- src/main/resources/db/migration/V7.8__alter_notification_types_table.sql
+++ src/main/resources/db/migration/V7.8__alter_notification_types_table.sql
@@ -3,3 +3,6 @@ USE
 
 ALTER TABLE notification_types
     MODIFY COLUMN action ENUM('NEW_REPORT', 'COMPLETE_REPORT', 'NEW_STREAK_ACHIEVEMENT');
+
+INSERT INTO notification_types (action, description)
+VALUES ('NEW_STREAK_ACHIEVEMENT', 'User achieved a new streak milestone');
