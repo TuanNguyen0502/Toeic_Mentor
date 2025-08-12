c7bf062bbd83f939cdb8df0313c87ade6ccb51c9
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-08-12 11:02:58 +0700

    feat: add role notifications for new streak achievement and streak revocation actions

--- src/main/resources/db/migration/V7.11__alter_notification_types_table.sql
+++ src/main/resources/db/migration/V7.11__alter_notification_types_table.sql
@@ -6,3 +6,10 @@ ALTER TABLE notification_types
 
 INSERT INTO notification_types (action, description)
 VALUES ('STREAK_ACHIEVE_REVOKED', 'User had a milestone revoked');
+
+INSERT INTO role_notifications (role_id, notification_type_id)
+SELECT r.id, nt.id
+FROM roles r
+         JOIN notification_types nt
+              ON nt.action = 'STREAK_ACHIEVE_REVOKED'
+WHERE r.name = 'ROLE_USER';
--- src/main/resources/db/migration/V7.8__alter_notification_types_table.sql
+++ src/main/resources/db/migration/V7.8__alter_notification_types_table.sql
@@ -6,3 +6,10 @@ ALTER TABLE notification_types
 
 INSERT INTO notification_types (action, description)
 VALUES ('NEW_STREAK_ACHIEVEMENT', 'User achieved a new streak milestone');
+
+INSERT INTO role_notifications (role_id, notification_type_id)
+SELECT r.id, nt.id
+FROM roles r
+         JOIN notification_types nt
+              ON nt.action = 'NEW_STREAK_ACHIEVEMENT'
+WHERE r.name = 'ROLE_USER';
