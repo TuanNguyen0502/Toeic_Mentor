b51aee3481aa7ae6abfe180c8d346b0c1bd88806
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-08-12 10:09:42 +0700

    refactor: rename migration files to maintain sequential order

--- src/main/java/intern/nhhtuan/toeic_mentor/repository/UserSettingRepository.java
+++
@@ -1,7 +0,0 @@
-package intern.nhhtuan.toeic_mentor.repository;
-
-import intern.nhhtuan.toeic_mentor.entity.UserSetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSettingRepository extends JpaRepository<UserSetting, Long> {
-}
