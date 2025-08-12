ade9db8f0fd4184221a41ca76e6941e88b64b89f
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-08-12 10:27:29 +0700

    feat: update study streaks and achievements tables to use DATE type for date fields

--- src/main/resources/db/migration/V7.5__create_streak_histories_table.sql
+++ src/main/resources/db/migration/V7.5__create_streak_histories_table.sql
@@ -4,15 +4,10 @@ USE
 CREATE TABLE streak_histories
 (
     id           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id      BIGINT   NOT NULL,
-    start_streak DATETIME NOT NULL,
-    end_streak   DATETIME,
+    user_id      BIGINT NOT NULL,
+    start_streak DATE   NOT NULL,
+    end_streak   DATE,
     created_at   DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at   DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
-
-INSERT INTO streak_histories (user_id, start_streak)
-SELECT id, '2025-07-28 14:44:36'
-FROM users
-WHERE id NOT IN (SELECT user_id FROM streak_histories);
