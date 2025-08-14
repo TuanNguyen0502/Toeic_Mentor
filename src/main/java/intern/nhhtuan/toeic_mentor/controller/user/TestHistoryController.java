813876e67e6c15be789b4971638db48250e631c8
Nguyen Ha Hong Tuan <nhhtuan-batch48@sdc.edu.vn>
2025-08-14 16:05:41 +0700

    feat: add filter for test history by completion status

--- src/main/java/intern/nhhtuan/toeic_mentor/controller/user/TestHistoryController.java
+++ src/main/java/intern/nhhtuan/toeic_mentor/controller/user/TestHistoryController.java
@@ -2,7 +2,6 @@ package intern.nhhtuan.toeic_mentor.controller.user;
 
 import intern.nhhtuan.toeic_mentor.dto.response.TestHistoryDetailResponse;
 import intern.nhhtuan.toeic_mentor.dto.response.TestHistoryResponse;
-import intern.nhhtuan.toeic_mentor.dto.response.TestResultResponse;
 import intern.nhhtuan.toeic_mentor.service.interfaces.ITestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -28,6 +27,7 @@ public class TestHistoryController {
             Model model,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime createdAtStart,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime createdAtEnd,
+            @RequestParam(required = false) Boolean completed,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "createdAt") String sortBy,
@@ -38,7 +38,7 @@ public class TestHistoryController {
             Authentication authentication = org.springframework.security.core.context.SecurityContextHolder.getContext().getAuthentication();
             String email = authentication.getName();
             testHistoryResponses = testService.getTestHistoryResponses(
-                    email, createdAtStart, createdAtEnd, page, size, sortBy, direction
+                    email, createdAtStart, createdAtEnd, completed, page, size, sortBy, direction
             );
         } catch (IllegalArgumentException e) {
             model.addAttribute("errorMessage", e.getMessage());
